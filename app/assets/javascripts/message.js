cccbc900c12c622029d0d1e417a7080326360584
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-15 17:08:19 +0900

    修正
    ・json_htmlとview_htmlの統一
    ・連続投稿可に
    ・messeage_contentのvalueを空に

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,27 +1,31 @@
 $(function() {
   function buildHTML(message) {
-    var imgge = message.image? `<img class="lower-message__image" src="${message.imag}">}` : "";
-    var html =
-      `<ul class="upper-message">
-        <li class="name">
-          ${message.name}
-        </li>
-        <li class="created_at">
-          ${message.created_at}
-        </li>
-      <div class="lower-message__content">
-        <p>${message.content}</p>
-        <div>${message.image}</div>
-      </div>`
+    var image = message.image.present? `<img class="lower-message__image" src="${message.imag}">}` : "";
+    var html =`
+      <div class = "main-content__message-history__message-box">
+        <ul class="upper-message">
+          <li class="name">
+            ${message.name}
+          </li>
+          <li class="created_at">
+            ${message.created_at}
+          </li>
+        <div class="lower-message__content">
+          <p>${message.content}</p>
+          <div>${image}</div>
+        </div>
+      </div>
+      `
 
     return html;
   }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
+    var href = $(this).attr('action');
     var formData = new FormData(this);
     $.ajax({
-      url: './messages',
+      url: href,
       type: "POST",
       data: formData,
       dataType: 'json',
@@ -31,10 +35,11 @@ $(function() {
 
     .done(function(data){
       var html = buildHTML(data);
-      $('.main-content__message-history__message-box').append(html)
-      $('#new_message').val('')
+      $('.main-content__message-history').append(html)
+      $('#message_content').val('')
       $('.main-content__message-history').animate({scrollTop: $('.main-content__message-history')[0].scrollHeight},'fast');
       $('#new_message')[0].reset();
+      $('.main-content__message-submit-box__send').prop('disabled', false);
     })
     .fail(function(){
       alert('error');
