a378d54e4f3f139edb80e8303b5f8b36a61a9a7e
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-15 12:33:11 +0900

    done処理-false処理

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,19 @@
+$(function() {
+  function buildHTML(message) {
+    var html =   `<ul class="upper-message">
+                    <li class="name">
+                      ${message.name}
+                    </li>
+                    <li class="created_at">
+                      ${message.created_at}
+                    </li>
+                  <div class="lower-message__content">
+                      ${message.content}
+                      ${message.image}
+                  </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var $form = $(this);
@@ -11,3 +27,19 @@
       processData: false,
       contentType: false
     })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-content__message-history__message-box').append(html)
+      $('#new_message').val('')
+      $('.main-content__message-history').animate({scrollTop: $('.main-content__message-history')[0].scrollHeight},'fast');
+      $('#new_message')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(function(){
+    })
+  })
+});
+
