bd39f600add3381bb795cbde1c2bd43fb47fe692
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-16 15:02:03 +0900

    add_java-script-html_add-btn

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -9,6 +9,9 @@ function appendUser(user) {
       <p class='chat-group-user__name'>
         ${user.name}
       </p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">
+      追加
+      </a>
     </div>
            `
     search_list.append(html);
@@ -23,11 +26,11 @@ function appendNoUser(user) {
     search_list.append(html);
 }
 
-   $("#user-search-field").on("keyup", function() {
-     var input = $(this).val();
-     console.log(input);
+  $("#user-search-field").on("keyup", function() {
+    var input = $(this).val();
+    console.log(input);
 
-     $.ajax({
+    $.ajax({
       type: 'GET',
       url: '/users',
       data: { keyword: input },
@@ -50,3 +53,5 @@ function appendNoUser(user) {
     })
   })
 })
+
+
