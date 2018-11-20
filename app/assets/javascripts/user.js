ce0870490c495e1e1e963937d98163a8a81666de
KEITA TAIRA <kt10580@hotmail.co.jp>
2018-11-20 19:31:31 +0900

    中間テーブル登録可能

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,6 +20,7 @@ function appendUser(user) {
 function appendAddUser( user_id, user_name) {
   var html = `
     <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user_id}'>
+      <input name='group[user_ids][]' type='hidden' value='${user_id}', mutiple:"true">
       <p class='chat-group-user__name'>${user_name}</p>
       <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
   </div>
